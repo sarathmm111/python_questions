210e259c49d5f82941c8f07877ea920bed222213
sarath <sarathmm224@gmail.com>
2023-10-31 20:53:49 +0530

    Added test code for occurance of a string

--- test.py
+++ test.py
@@ -19,3 +19,27 @@ import unittest
          
  if __name__ == '__main__':
      unittest.main()
+     
+def freq(s):
+    frequency = {}
+    for i in s:
+        if i in frequency:
+            frequency[i] += 1
+        else:
+            frequency[i] = 1
+    return frequency
+    
+ class TestFreq(unittest.TestCase):
+ 
+    def testIsFreq(self):
+        sentence = "hai i am sarath"
+        self.assertTrue(freq(sentence))
+   
+    def testNullInput(self):
+        ret = freq("")
+        self.assertFalse(ret)
+
+        
+
+if __name__ == '__main__':
+    unittest.main()
